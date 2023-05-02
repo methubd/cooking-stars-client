ebd634f32c4eb5460d22a33a83d8dc467a9bc97b
T R Methu <fmethubd@gmail.com>
2023-05-02 11:18:07 +0600

    chef data loaded

--- src/components/Shared/Home/Home.jsx
+++ src/components/Shared/Home/Home.jsx
@@ -1,13 +1,24 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Banner from '../Banner/Banner';
 import ChefProfile from '../ChefProfile/ChefProfile';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const data = useLoaderData();
+    console.log(data);
     return (
         <div>
             <Banner></Banner>
-            <ChefProfile></ChefProfile>
+            <div className='grid md:grid-cols-3 w-3/4 mx-auto gap-5 py-10'>
+                {
+                    data.chefs.map(chef => <ChefProfile 
+                        chef={chef} 
+                        key={chef.chef_id}
+                        ></ChefProfile>)
+                }
+            </div>
         </div>
     );
 };
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../components/Shared/Home/Home";
+import ChefProfile from "../components/Shared/ChefProfile/ChefProfile";
 
 // eslint-disable-next-line no-unused-vars
 const router = createBrowserRouter ([
@@ -10,7 +11,12 @@ const router = createBrowserRouter ([
         children: [
             {
                 path: '/',
-                element: <Home></Home>
+                element: <Home></Home>,
+                loader: () => fetch('http://localhost:5000/chefs')
+            }, 
+            {
+                path: '/chef-profile',
+                element: <ChefProfile></ChefProfile>
             }
         ]
     }
